e06002eeb39577700e1e6642a3b58e32a24cf086
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-09 20:31:14 -0300

    agregado de componente Tarea a App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Tarea from './Components/Tarea';
 
 function App() {
   return (
     <div className="App">
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
-
+        <Tarea texto="Ejemplo de texto"/>
       </div>
     </div>
   );
