2a912c2ebd15e323667eaf241a5d2174056cab70
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-08 22:51:23 -0300

    Recuadro tareas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+
+      </div>
     </div>
   );
 }
