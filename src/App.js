6e3ca3c8cb05e3a327d36366ffeaaf3f9f4898b8
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-10 23:29:44 -0300

    Creación y agregado de componente formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Tarea from './Components/Tarea';
+import TareaFormulario from './Components/TareaFormulario';
 
 function App() {
   return (
     <div className="App">
       <div className='tareas-lista-principal'>
         <h1>Mis Tareas</h1>
+        <TareaFormulario />
         <Tarea texto="Ejemplo de texto" completada={true}/>
       </div>
     </div>
