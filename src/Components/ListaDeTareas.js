e9cfbdf74e0e7d35d29b1e1029cea52262b86180
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-11 22:49:32 -0300

    Create ListaDeTareas.js

---
+++ src/Components/ListaDeTareas.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TareaFormulario from "./TareaFormulario";
+
+function ListaDeTareas() {
+    return(
+        <>
+            <TareaFormulario />
+            <div className="tareas-lista-contenedor">
+                LISTA DE TAREAS
+            </div>
+        </>
+    );
+}
+export default ListaDeTareas;
