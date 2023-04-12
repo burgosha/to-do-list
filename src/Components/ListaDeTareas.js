8b5ae781d041dac94db3fca96a6521d89b855744
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-11 22:58:55 -0300

    Agregado de función de renderizado a partir de hook

--- src/Components/ListaDeTareas.js
+++ src/Components/ListaDeTareas.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import TareaFormulario from "./TareaFormulario";
+import Tarea from "./Tarea";
 import "../Stylesheets/ListaDeTareas.css";
 
 function ListaDeTareas() {
+
+    // Incialmente tareas es un arreglo vacío (estado inicial)
+    const [tareas, setTareas] = useState([]);
+
     return(
         <>
             <TareaFormulario />
             <div className="tareas-lista-contenedor">
-                LISTA DE TAREAS
+                {
+                    tareas.map(
+                        (tarea) => 
+                            <Tarea
+                                texto={tarea.texto}
+                                completada={tarea.completada}
+                            />
+                    )
+                }
             </div>
         </>
     );
